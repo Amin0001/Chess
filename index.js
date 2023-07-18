971969ad0dec3d3a91add1b5445605950e3d6a3b
Aminu Muhammed-lawal <116843719+Amin0001@users.noreply.github.com>
2023-07-18 06:32:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,47 @@ io.on('connection', function(socket) {
        socket.broadcast.emit('move', msg); 
     });
 });
+// functions/default.js
+
+// The handler function for the root endpoint
+exports.handler = async (event, context) => {
+  try {
+    // Assuming you want to serve the 'default.html' file from the 'public' folder
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: '<html><body><h1>Hello, Serverless World!</h1></body></html>',
+    };
+
+    return response;
+  } catch (error) {
+    // Handle any errors
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error' }),
+    };
+  }
+};
+// functions/move.js
+
+exports.handler = async (event, context) => {
+  try {
+    // Assuming you want to broadcast the 'move' event message to clients
+    const { msg } = JSON.parse(event.body);
+
+    // Perform any processing or validation on the message if needed
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ status: 'success' }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
+
